c585368ee1fd61042062768dab9b31796fb5e6c9
dhzdhd <ragas5432100@gmail.com>
2025-02-15 12:13:24 +0530

    Add theme mode switcher

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ThemeSwitcher from './ThemeSwitcher.svelte';
+</script>
+
+<header
+	class="bg-accent fixed z-20 flex h-16 w-screen items-center justify-between border-b-[0.1rem] border-solid px-2"
+>
+	<h1>Title</h1>
+	<div>
+		<ThemeSwitcher />
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
 	import type { Snippet } from 'svelte';
 	import type { LayoutServerData } from './$types';
+	import Header from '$lib/components/layout/Header.svelte';
 
 	let { children, data }: { children: Snippet<[]>; data: LayoutServerData } = $props();
 	console.log(data);
 </script>
 
+<ModeWatcher />
+<Header />
 {@render children()}
