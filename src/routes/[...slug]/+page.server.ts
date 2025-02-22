5e4445b9e85cf1ea810d49c270fed2ed6246a7b4
dhzdhd <ragas5432100@gmail.com>
2025-02-22 14:47:04 +0530

    Fix prerendering on dynamic routes

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -1,5 +1,18 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
+import type { EntryGenerator } from './$types';
+
+export const entries: EntryGenerator = async () => {
+    const rawPosts = Object.entries(import.meta.glob<any>('../../../posts/**.md', { query: '?raw' }),);
+    const getSlug = (fileName: string): string => {
+        return fileName.replace('.md', '').replace('../../../posts/', '');
+    }
+
+    return rawPosts.map(([fileName]) => {
+        return { slug: getSlug(fileName) }
+    })
+};
+
 
 export const load: PageServerLoad = async ({ params, parent }) => {
     const slug = params.slug
