cd6746a02ca19510769bc3d132bb834b38b6d786
dhzdhd <dhzdhd@proton.me>
2025-11-04 00:31:14 +0530

    Remove mdsvex patch and dep

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter()
 	},
-	extensions: ['.svelte', '.svx', '.md']
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
