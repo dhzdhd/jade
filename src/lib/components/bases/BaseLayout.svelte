a363600aab02c86432375b7580d6358aa4578bb2
dhzdhd <72333092+dhzdhd@users.noreply.github.com>
2025-11-09 15:15:51 +0000

    Deploying to deploy from @ dhzdhd/jade@74182b3ea9db33edf0dd3c10b4d413f3b225c35f 🚀

---
+++ src/lib/components/bases/BaseLayout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Bases } from '$lib/post';
+
+	interface BaseLayoutProps {
+		data: Bases;
+	}
+
+	const props: BaseLayoutProps = $props();
+</script>
+
+<div></div>
