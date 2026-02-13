5fd17d4ed8f84ef73261ed54db094727eede41d1
dhzdhd <dhzdhd@proton.me>
2026-02-13 22:14:20 +0530

    Add id to image node

--- src/lib/components/canvas/ImageNode.svelte
+++ src/lib/components/canvas/ImageNode.svelte
@@ -9,11 +9,11 @@
 <div class="image-node">
 	<img src={nodeData.content} alt={nodeData.content} />
 </div>
-<Handle type="target" position={Position.Top} />
-<Handle type="target" position={Position.Left} />
-<Handle type="target" position={Position.Right} />
-<Handle type="target" position={Position.Bottom} />
-<Handle type="source" position={Position.Top} />
-<Handle type="source" position={Position.Left} />
-<Handle type="source" position={Position.Right} />
-<Handle type="source" position={Position.Bottom} />
+<Handle type="target" position={Position.Top} id="top" />
+<Handle type="target" position={Position.Left} id="left" />
+<Handle type="target" position={Position.Right} id="right" />
+<Handle type="target" position={Position.Bottom} id="bottom" />
+<Handle type="source" position={Position.Top} id="top" />
+<Handle type="source" position={Position.Left} id="left" />
+<Handle type="source" position={Position.Right} id="right" />
+<Handle type="source" position={Position.Bottom} id="bottom" />
