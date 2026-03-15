961f91f3f7737235a5b40b11570d5c4bb49bb687
dhzdhd <dhzdhd@proton.me>
2026-03-15 23:28:05 +0530

    Add some build changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-
+	use: {
+		args: ['--no-sandbox', '--disable-setuid-sandbox']
+	},
 	testDir: 'e2e'
 });
